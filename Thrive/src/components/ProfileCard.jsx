90d1c43d944749b00b47d1c219c1ee27162a5f25
MariiaaKadhim <mariiaa.sk@hotmail.com>
2024-07-09 10:14:06 +0300

    fetch the data for the profile page

--- Thrive/src/components/ProfileCard.jsx
+++ Thrive/src/components/ProfileCard.jsx
@@ -1,8 +1,17 @@
 import React from "react"
+import { useState, useEffect } from "react"
+import Client from "../services/api"
+
 const profileCard = ({ profile }) => {
   const { userName, birthDate, nationality, gender, email, phoneNumber } =
     profile
 
+  const [profils, setProfils] = useState()
+  useEffect(() => {
+    fetch("/:userId").then((response) => {
+      setProfils(response.json())
+    })
+  }, [])
   return (
     <div className="profileCard">
       <h2>My Profile</h2>
