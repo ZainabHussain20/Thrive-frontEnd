1dd2c5a1a7d85345c2e2a8f9b70c8ed9d59c9661
zainabtalaq <zainab.altallaq@gmail.com>
2024-07-10 11:50:26 +0300

    add pages

--- Thrive/src/pages/MyRequest.jsx
+++ Thrive/src/pages/MyRequest.jsx
@@ -1,15 +1,41 @@
-import React from "react"
-import ProgramRequest from "../components/ProgramsRequest"
+import React, { useState, useEffect } from "react"
+import MyProgramCard from "../components/MyProgramCard"
+import axios from "axios"
+import { BASE_URL } from "../services/api"
+import { Link } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
-const MyRequests = () => {
-  const userRequests = []
+const MyPrograms = () => {
+  const [programs, setPrograms] = useState([])
+  let { userId } = useParams()
+
+  useEffect(() => {
+    const getPrograms = async () => {
+      try {
+        const res = await axios.get(`${BASE_URL}/registration/${userId}`)
+        setPrograms(res.data)
+      } catch (err) {
+        console.log("Error fetching programs:", err)
+      }
+    }
+    getPrograms()
+  }, [])
 
   return (
     <div>
-      <h1>My Requests</h1>
-      <ProgramRequest userRequests={userRequests} />
+      {programs.length > 0 ? (
+        programs.map((program) => (
+          <MyProgramCard
+            key={program._id}
+            id={program._id}
+            name={program.name}
+          />
+        ))
+      ) : (
+        <h4>No programs available!</h4>
+      )}
     </div>
   )
 }
 
-export default MyRequests
+export default MyPrograms
